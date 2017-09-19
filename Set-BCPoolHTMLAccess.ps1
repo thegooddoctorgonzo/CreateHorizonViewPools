354bfc9471968faa7a5a4a8ca935e96562234273
thegooddoctorgonzo <steven.m.landry@gmail.com>
2017-09-19 09:38:37 -0500

    Add files via upload

---
+++ Set-BCPoolHTMLAccess.ps1
@@ -0,0 +1,67 @@
+﻿function Set-BCPoolHtmlAccess ([string]$Pool_id, [boolean]$HtmlAccess) {  
+<#  
+    .SYNOPSIS  
+        enables or disables HTML Access to the given VMware View desktop pool  
+    .DESCRIPTION  
+        This function enable or disable HTML Access to a desktop pool, by modifying  
+        the "pae-ServerProtocolLevel" property of the associated object in the ADAM   
+        database via LDAP. This property is a multi-valued attribute contains a array   
+        of string, which designates   
+        by which protocol desktops can be accessed. It valid values are "PCOIP",  
+        "RDP" and "BLAST". Controlling the existance of the string "BLAST" determines  
+        if the pool is accessible through HTML Access. The parameter Pool_id determines  
+        which object is modified.  
+    .EXAMPLE  
+        Set-BCPoolHtmlAccess "W7ST620" $True     
+    .PARAMETER Pool_id  
+        the pool_id of the desktop pool of which to enable or disable HTML access. Wildcards are allowed!  
+    .PARAMETER HtmlAccess  
+        Boolean value to set the HTML Access to.  
+    .OUTPUT  
+        None  
+    .NOTES  
+        Written by Paul Wiegmans on 31-8-2014  
+        "pae-ServerProtocolLevel" is a multivalued attribute, which is a little difficult to   
+        write correctly to the object.  
+        http://www.selfadsi.org/write.htm   
+                Google "powershell ldap multi value property"  
+        http://jdhitsolutions.com/blog/2011/12/updating-multi-valued-active-directory-properties-part-1/  
+#>  
+    $dn = "DC=vdi,DC=vmware,DC=int"   # root OU of VMware View ADAM database (CUSTOMIZE ME)   
+    $domain = "LDAP://localhost:389/$dn"  # connect to the ADAM database (CUSTOMIZE ME)   
+      
+    $root = New-Object System.DirectoryServices.DirectoryEntry $domain  
+    $query = New-Object System.DirectoryServices.DirectorySearcher  
+    $query.searchroot = $root  
+    $query.filter = "(&(objectCategory=pae-DesktopApplication)(cn=$pool_id))"  
+    $pools = @($query.findall())  
+    $propname = "pae-ServerProtocolLevel"  
+      
+    foreach ($pool in $pools) {      
+          
+        $poolobj = [ADSI]$pool.GetDirectoryEntry()  
+        $protocols = $poolobj.$propname   
+        $desiredprotocols = @()       
+        foreach ($protocol in $protocols) {  
+            if ($protocol -ne "BLAST") {  
+                $desiredprotocols += $protocol  # save a list of all existing protocols   
+            }  
+        }    
+          
+        if ($HtmlAccess) {          
+            $desiredprotocols += "BLAST"  
+        }  
+        write-verbose ("Desktop pool " + $poolobj.name + " gets protocols: "+$desiredprotocols)  
+        $poolobj.$propname = $desiredprotocols     
+        #$poolobj.$propname = @("PCOIP","RDP","BLAST")  # to reset to normal values  
+        $poolobj.CommitChanges()  
+    }       
+   
+}  
+  
+  
+  
+  
+#$pool_id = "W7S*"                  # pool_id of pool to set protocols of   
+#Set-BCPoolHtmlAccess $pool_id $false  
+#Set-BCPoolHtmlAccess $pool_id $true 
---
+++ Set-PoolBlackoutTimes.ps1
@@ -0,0 +1,60 @@
+﻿ 
+
+
+function Set-PoolBlackoutTimes ([string]$Pool_id, [string]$StartTime, [string]$StopTime, [System.Array]$days) {  
+<#  
+    .SYNOPSIS  
+        Sets the Cache Refresh Blackout times for the given VMware View desktop pool  
+    .DESCRIPTION  
+        This function sets the Cache Refresh Blackout times for a Horizon View pool. Clears any current entries.
+        This is controlled in the ADAM DB. The attribute is pae-CBRCCacheRefreshBlackout. Sets the days and times
+    .EXAMPLE  
+        Set-PoolBlackoutTimes -Pool_id TESTTEST -StartTime "07:00" -StopTime "23:00" -days 1,2,3,4,5   
+    .PARAMETER StartTime
+        This is the start time of the blackout time. Use format "07:00". Default time is 07:00 AM 
+    .PARAMETER StopTime
+        This is the stop time of the blackout time. Use format "07:00". Default time is 23:00 PM
+    .PARAMETER days
+        Days to use blackout time. Input is the number of the day of the week (Mon=1, Sun=7), seperated by commas, in format 1,2,3,4,5   
+    .OUTPUT  
+        None  
+    .NOTES  
+        Written by Steve Landry 20170807. 
+        
+        Reading values out has to be done in the format **$flags =  $poolobj.Properties.'pae-serverpoolflags'.Value**. Writing
+        is in the format **$poolobj.'pae-serverpoolflags'.Add()**. 
+
+        Starting point for this was a function called Set-BCPoolHtmlAccess written by Paul Wiegmans on 31-8-2014. 
+#>  
+    
+   
+         
+    $Searcher = New-Object DirectoryServices.DirectorySearcher
+    $Searcher.SearchRoot = 'LDAP://localhost:389/dc=vdi,dc=vmware,dc=int'
+    $Searcher.Filter = "(&(objectCategory=pae-ServerPool)(anr=$pool_id))"
+
+    if(!($StartTime.Length -eq 5 -and $StartTime.Contains(":")))
+    {
+        $StartTime = "07:00"
+        $StopTime = "23:00"
+    }
+    
+     foreach ($poolObj in [ADSI]$Searcher.FindAll().GetDirectoryEntry())
+     {   
+        #if the pool has values in the property
+        if($poolobj.Properties.'pae-CBRCCacheRefreshBlackout'.Value)
+        {
+          $poolObj.Properties.'pae-CBRCCacheRefreshBlackout'.Clear()
+        }
+
+        foreach($day in $days)
+        {
+            $minutes = ([int]$StopTime.Replace(":00","") - [int]$StartTime.Replace(":00","")) * 60
+            [string]$value = $day.ToString() + ":" + $StartTime + ";" + $minutes.ToString()
+            $poolobj.'pae-CBRCCacheRefreshBlackout'.Add($value)
+        }
+
+        $poolobj.CommitChanges()
+    } 
+}  
+  
